bff69851898ea252ab8d880b1229a11ca920faf7
Unknown <llairunhao@gmail.com>
2018-10-25 21:02:26 +0800

    语音页面

---
+++ LaoRenJi/ViewController/Service/Controller/Chat/ChatHUD.m
@@ -0,0 +1,21 @@
+//
+//  ChatHUD.m
+//  LaoRenJi
+//
+//  Created by RunHao on 2018/10/25.
+//  Copyright © 2018年 西汉科技. All rights reserved.
+//
+
+#import "ChatHUD.h"
+
+@implementation ChatHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
